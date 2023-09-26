03a386ba34b22ddbda961990d8454d2278059e66
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-09-26 15:55:35 +0300

    implement insertion operation

---
+++ practice2/apache/insert.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert</title>
+</head>
+<body>
+    <form method="POST">
+        <input type="text" placeholder="NAME" required="true" name="name">
+        <br>
+        <input type="text" placeholder="PRICE" required="true" name="price">
+        <br>
+        <button type="submit" name="submit">Insert</button>
+    </form>
+<?php 
+if(isset($_POST['submit'])) {
+  	//getting the post values
+    $name=$_POST['name'];
+    $price=$_POST['price'];
+
+    $mysqli = new mysqli("db", "user", "password", "appDB");
+
+    $query = $mysqli->query("INSERT INTO products(name, price) VALUES ('$name', $price)");
+
+    if ($query) {
+        echo "<script>alert('You have successfully inserted the data');</script>";
+        echo "<script type='text/javascript'> document.location ='index.php'; </script>";
+    } else {
+        echo "<script>alert('Something Went Wrong. Please try again');</script>";
+    }
+}
+
+?>
+</body>
+</html>
