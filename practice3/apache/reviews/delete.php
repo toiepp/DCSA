019a6e68a989b1acf272c69871240f30a2e406bf
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-10-11 14:39:34 +0300

    add deleting function

--- practice3/apache/reviews/delete.php
+++ practice3/apache/reviews/delete.php
@@ -6,7 +6,7 @@ global $entity;
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
-    $deleted_entity = $mysqli->query("SELECT * FROM $entity WHERE id=$id")->fetch_assoc();
+    $deleted_entity = $mysqli->query("SELECT * FROM reviews WHERE id=$id")->fetch_assoc();
 
     if (!$deleted_entity) {
         echo json_encode([
@@ -14,7 +14,7 @@ if (isset($_GET['id'])) {
             'code' => '400 HTTP Bad Request'
         ]);
     } else {
-        $query = $mysqli->query("DELETE FROM $entity WHERE id=$id");
+        $query = $mysqli->query("DELETE FROM reviews WHERE id=$id");
         echo json_encode($deleted_entity);
     }
 
