cf16133de0a37e1182a2625772a589976472fb9f
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-10-11 15:37:48 +0300

    finish practice3

--- practice3/apache/products/post.php
+++ practice3/apache/products/post.php
@@ -8,10 +8,10 @@ $product = json_decode(file_get_contents('php://input'), false);
 $instruction = null;
 
 if (isset($product->id)) {
-    $instruction = "REPLACE INTO $entity (id, name, price) " .
+    $instruction = "REPLACE INTO products (id, name, price) " .
         "VALUE ($product->id, '$product->name', $product->price)";
 } else {
-    $instruction = "INSERT INTO $entity (" .
+    $instruction = "INSERT INTO products (" .
         (isset($product->id) ? "id," : "") .
         "name, price) VALUES (" . 
         (isset($product->id) ? $product->id . ", " : "") .
@@ -21,7 +21,7 @@ if (isset($product->id)) {
 $query = $mysqli->query($instruction);
 
 if ((int)$query == TRUE) {
-    echo json_encode($mysqli->query("SELECT * FROM $entity WHERE id={$mysqli->insert_id}")->fetch_assoc());
+    echo json_encode($mysqli->query("SELECT * FROM products WHERE id={$mysqli->insert_id}")->fetch_assoc());
 } else {
     echo json_encode([
         "message" => "Something went wrong",
--- practice3/apache/reviews/post.php
+++ practice3/apache/reviews/post.php
@@ -3,25 +3,32 @@ include("../config.php");
 
 global $mysqli;
 
-$product = json_decode(file_get_contents('php://input'), false);
+$review = json_decode(file_get_contents('php://input'), false);
 
 $instruction = null;
 
-if (isset($product->id)) {
-    $instruction = "REPLACE INTO $entity (id, name, price) " .
-        "VALUE ($product->id, '$product->name', $product->price)";
-} else {
-    $instruction = "INSERT INTO $entity (" .
-        (isset($product->id) ? "id," : "") .
-        "name, price) VALUES (" . 
-        (isset($product->id) ? $product->id . ", " : "") .
-        "'$product->name', $product->price)";
+if (isset($review->id)) {
+
+    $persisted_review = $mysqli->query("SELECT * FROM reviews WHERE id={$review->id}")->fetch_assoc();
+
+    $instruction = "REPLACE INTO reviews (id, author, content, product_id)
+            VALUE ({$persisted_review['id']},
+            '{$persisted_review['author']}',
+            '{$review->content}',
+            {$persisted_review['product_id']})";
+
+} else { // if you want to insert new entity =====OK=====
+    $instruction = "INSERT INTO reviews (" .
+        (isset($review->id) ? "id," : "") .
+        "author, content, product_id) VALUES (" . 
+        (isset($review->id) ? $review->id . ", " : "") .
+        "'$review->author', '$review->content', $review->product_id)";
 }
 
 $query = $mysqli->query($instruction);
 
 if ((int)$query == TRUE) {
-    echo json_encode($mysqli->query("SELECT * FROM $entity WHERE id={$mysqli->insert_id}")->fetch_assoc());
+    echo json_encode($mysqli->query("SELECT * FROM reviews WHERE id={$mysqli->insert_id}")->fetch_assoc());
 } else {
     echo json_encode([
         "message" => "Something went wrong",
