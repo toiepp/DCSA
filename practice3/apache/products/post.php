736500baa890edabd16b308f35d82412a2f5566a
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-10-11 13:33:54 +0300

    add updating in post.php

--- practice3/apache/products/post.php
+++ practice3/apache/products/post.php
@@ -5,18 +5,23 @@ global $mysqli;
 
 $product = json_decode(file_get_contents('php://input'), false);
 
+$instruction = null;
 
-
-$instruction = "INSERT INTO $entity (" .
-            (isset($product->id) ? "id," : "") .
-            " name, price) VALUES (" . 
-            (isset($product->id) ? $product->id . ", " : "") .
-            "'$product->name', $product->price)";
+if (isset($product->id)) {
+    $instruction = "REPLACE INTO $entity (id, name, price) " .
+        "VALUE ($product->id, '$product->name', $product->price)";
+} else {
+    $instruction = "INSERT INTO $entity (" .
+        (isset($product->id) ? "id," : "") .
+        "name, price) VALUES (" . 
+        (isset($product->id) ? $product->id . ", " : "") .
+        "'$product->name', $product->price)";
+}
 
 $query = $mysqli->query($instruction);
 
 if ((int)$query == TRUE) {
-    echo json_encode($product);
+    echo json_encode($mysqli->query("SELECT * FROM $entity WHERE id={$mysqli->insert_id}")->fetch_assoc());
 } else {
     echo json_encode([
         "message" => "Something went wrong",
