6adebc356cbcf3af9c97ea5ef178746093939efa
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-11-06 12:08:10 +0300

    fix(entity): add json ignoring of hidden property "hibernateLazyInitializer"

--- practice6/src/main/java/me/mikholsky/practice6/entity/CartRow.java
+++ practice6/src/main/java/me/mikholsky/practice6/entity/CartRow.java
@@ -1,12 +1,15 @@
 package me.mikholsky.practice6.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity(name = "Cart")
 @Table(name = "user_products")
 @Data
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class CartRow {
     @EmbeddedId
     private CartRowId id;
